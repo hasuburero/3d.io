a9a1a5680609d90de9a3d65c5f6124c2482418d4
hasuburero <hasuburero0802@gmail.com>
2022-04-23 13:02:15 +0900

    test commit

--- index.js
+++ index.js
@@ -12,20 +12,26 @@ function init(){
 	const camera = new THREE.PerspectiveCamera(45, 800/600, 1, 10000);
 
 	const geometry = new THREE.BoxGeometry(500, 500, 500);
-
 	const material = new THREE.MeshStandardMaterial({
 		color: 0x0000ff
 	});
-
 	const box = new THREE.Mesh(geometry, material);
 	scene.add(box);
 
 	const light = new THREE.DirectionalLight(0xffffff);
 	light.intensity=2;
+	light.position.set(1, 1, 1);
 	scene.add(light);
 
-	light.position.set(1, 1, 1);
+	tick();
 
+	function tick(){
+		requestAnimationFrame(tick);
 
+		box.rotation.x += 0.01;
+		box.rotation.y += 0.01;
+
+		renderer.render(scene, camera);
+	}
 }
 
